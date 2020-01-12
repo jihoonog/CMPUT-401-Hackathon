ad6264dc0b4a4a3e1694c2dc2fa216e55855561b
tcchan97 <tcchan@ualberta.ca>
2020-01-12 11:53:03 -0700

    full version

--- Frontend/react-d3/src/Charts.js
+++ Frontend/react-d3/src/Charts.js
@@ -1,29 +1,49 @@
 import React, { Component } from 'react';
 import './Charts.css'
-import BarChart from './BarChart';
+import CanvasJSChart from './canvasjs.react';
+
 
 class Charts extends Component {
-    state = {
-        data: [12, 5, 6, 6, 9, 10],
-        width: 500,
-        left: 500,
-        height: 200,
-        id: "root"
-    }
-
     render() {
-        return (
-            <div className="Charts">
-                <div className="barChart">
-                    <BarChart
-                        data={this.state.data} 
-                        width={this.state.width} 
-                        height={this.state.height} 
-                    />
-                </div>
-            </div>
-        )
-    }
+		const options = {
+			animationEnabled: true,
+			exportEnabled: true,
+			theme: "light2", //"light1", "dark1", "dark2"
+			title:{
+				text: "Simple Column Chart with Index Labels"
+			},
+			data: [{
+				type: "column", //change type to bar, line, area, pie, etc
+				//indexLabel: "{y}", //Shows y value on all Data Points
+				indexLabelFontColor: "#5A5757",
+				indexLabelPlacement: "outside",
+				dataPoints: [
+					{ x: 10, y: 71 },
+					{ x: 20, y: 55 },
+					{ x: 30, y: 50 },
+					{ x: 40, y: 65 },
+					{ x: 50, y: 71 },
+					{ x: 60, y: 68 },
+					{ x: 70, y: 38 },
+					{ x: 80, y: 92, indexLabel: "Highest" },
+					{ x: 90, y: 54 },
+					{ x: 100, y: 60 },
+					{ x: 110, y: 21 },
+					{ x: 120, y: 49 },
+					{ x: 130, y: 36 }
+				]
+			}]
+		}
+		
+		return (
+		<div>
+			<CanvasJSChart options = {options} 
+				/* onRef={ref => this.chart = ref} */
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
+		);
+	}
 }
 
 export default Charts;
